ac5adbaf213ef69260009fc392bd795e2341b1fa
Ryan Sims <ryanjsims@email.arizona.edu>
2017-11-13 23:25:45 -0700

    Added GroceryStore, Department, Inventory, and Log

---
+++ GroceryStore/src/store/objects/Inventory.java
@@ -0,0 +1,22 @@
+package store.objects;
+
+import java.util.ArrayList;
+
+public class Inventory {
+	private ArrayList<Product> products;
+
+	public Inventory() {
+		products = new ArrayList<Product>();
+	}
+	
+	public void addProduct(Product aProduct) {
+		products.add(aProduct);
+	}
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+	
+	public void updateProduct(Product aProduct) {
+		//slightly messy implementation. Maybe check for a product with identical name or ID then update remaining info?
+	}
+}
